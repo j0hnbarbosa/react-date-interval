ee32ee6ab4d2e82ea1e98a0db2762248e537f145
j0hnbarbosa <john.contatob@gmail.com>
2019-02-19 23:39:49 -0300

    adicionado link do semantic no index do html

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
 import React from 'react'
-import { Grid, Segment } from 'semantic-ui-react'
+import { Grid, Segment, Container } from 'semantic-ui-react'
 
 const GridExampleEqualWidthRow = () => (
-  <Grid columns='equal'>
-    <Grid.Row>
-      <Grid.Column>
-        <Segment>1</Segment>
-      </Grid.Column>
-      <Grid.Column>
-        <Segment>2</Segment>
-      </Grid.Column>
-      <Grid.Column>
-        <Segment>3</Segment>
-      </Grid.Column>
-      <Grid.Column>
-        <Segment>4</Segment>
-      </Grid.Column>
-    </Grid.Row>
-    <Grid.Row>
-      <Grid.Column>
-        <Segment>1</Segment>
-      </Grid.Column>
-      <Grid.Column>
-        <Segment>2</Segment>
-      </Grid.Column>
-      <Grid.Column>
-        <Segment>3</Segment>
-      </Grid.Column>
-    </Grid.Row>
-    <Grid.Row>
-      <Grid.Column>
-        <Segment>1</Segment>
-      </Grid.Column>
-      <Grid.Column>
-        <Segment>2</Segment>
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
+  <Container>
+    <Grid columns='equal'>
+      <Grid.Row>
+        <Grid.Column>
+          <Segment>1</Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>2</Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>3</Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>4</Segment>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column>
+          <Segment>1</Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>2</Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>3</Segment>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column>
+          <Segment>1</Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>2</Segment>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </Container>
 )
 
 export default GridExampleEqualWidthRow
