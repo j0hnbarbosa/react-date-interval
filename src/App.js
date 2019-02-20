6bace278070cfb700c4ca4e233e441eb7cc1e5d2
j0hnbarbosa <john.contatob@gmail.com>
2019-02-19 23:12:22 -0300

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Grid, Segment } from 'semantic-ui-react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const GridExampleEqualWidthRow = () => (
+  <Grid columns='equal'>
+    <Grid.Row>
+      <Grid.Column>
+        <Segment>1</Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Segment>2</Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Segment>3</Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Segment>4</Segment>
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column>
+        <Segment>1</Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Segment>2</Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Segment>3</Segment>
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column>
+        <Segment>1</Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Segment>2</Segment>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+)
 
-export default App;
+export default GridExampleEqualWidthRow
